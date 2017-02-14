beddf980f8afb5a3a817a75c44413bafb308526b
15221572751 <15221572751@163.com>
2017-02-14 20:19:20 +0800

    Initial commit

---
+++ src/main/java/com/mcf/Appmain.java
@@ -0,0 +1,10 @@
+package com.mcf;
+
+/**
+ * Created by admin on 2017/2/14.
+ */
+public class Appmain {
+    public static void main(String[] args) {
+        System.out.println("这是一个入口");
+    }
+}
